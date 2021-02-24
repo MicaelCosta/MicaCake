3337ba123486badc005ba22bcc4f2a3f0b882fb3
MicaelCosta <micaelcosta97@hotmail.com>
2021-02-24 10:04:32 -0300

    feat: improvement context

--- src/App.js
+++ src/App.js
@@ -1,102 +1,27 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import api from "./services/api";
-import { formatPrice } from "./util/format";
+import { ToastContainer } from "react-toastify";
 
 import { Header } from "./components";
 import Routes from "./routes";
 
 import GlobalStyle, { Container } from "./styles/global";
 
-import CartContext from "./Context/CartContext";
+import { CartContextProvider } from "./Context/CartContext";
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const removeFromCart = useCallback((id) => {
-    setCart(prevState => {
-      toast.success("Produto removido do carrinho!");
-
-      return prevState.filter(x => x.id !== id);
-    });
-  }, []);
-
-  const updateAmount = useCallback(async (id, amount) => {
-    if (amount === 0) {
-      removeFromCart(id);
-      return;
-    }
-
-    const { data: stock } = await api.get(`/stock/${id}`);
-    const stockAmount = stock.amount;
-
-    if (amount > stockAmount) {
-      toast.error("Quantidade solicitada fora do estoque!");
-      return;
-    }
-
-    setCart((prevState) => {
-      return prevState.map(item => ({
-        ...item,
-        amount: item.id === id ? amount : item.amount
-      }));
-    });
-  }, [removeFromCart]);
-
-  const addToCart = useCallback(async (id) => {
-    //Verifica se o produto já existe no carrinho
-    const productExists = cart.find((x) => x.id === id);
-
-    //consulta o stock antes de qualquer alteração
-    const { data: stock } = await api.get(`/stock/${id}`);
-
-    const stockAmount = stock.amount;
-    const currentAmount = productExists ? productExists.amount : 0;
-
-    const amount = currentAmount + 1;
-    if (amount > stockAmount) {
-      toast.error("Quantidade solicitada fora do estoque!");
-      return;
-    }
-
-    if (productExists) {
-      await updateAmount(id, amount);
-
-      toast.success("Produto adicionado ao carrinho!");
-    } else {
-      const response = await api.get(`/products/${id}`);
-
-      const data = {
-        ...response.data,
-        amount: 1, //insere quantidade 1
-        priceFormatted: formatPrice(response.data.price),
-      };
-
-      setCart(prevState => [...prevState, data]);
-
-      toast.success("Produto adicionado ao carrinho!");
-    }
-  }, [cart, updateAmount]);
 
   return (
     <BrowserRouter>
       <GlobalStyle />
 
-      <CartContext.Provider
-        value={{
-          cart,
-          addToCart,
-          updateAmount,
-          removeFromCart
-        }}
-      >
+      <CartContextProvider>
         <Header />
 
         <Container>
           <Routes />
         </Container>
-      </CartContext.Provider>
+      </CartContextProvider>
 
       <ToastContainer autoClose={3000} />
     </BrowserRouter>
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -1,5 +1,86 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { toast } from "react-toastify";
+import api from "../services/api";
+import { formatPrice } from "../util/format";
 
 const CartContext = React.createContext({});
 
-export default CartContext;
+export function CartContextProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    const removeFromCart = useCallback((id) => {
+        setCart(prevState => {
+            toast.success("Produto removido do carrinho!");
+
+            return prevState.filter(x => x.id !== id);
+        });
+    }, []);
+
+    const updateAmount = useCallback(async (id, amount) => {
+        if (amount === 0) {
+            removeFromCart(id);
+            return;
+        }
+
+        const { data: stock } = await api.get(`/stock/${id}`);
+        const stockAmount = stock.amount;
+
+        if (amount > stockAmount) {
+            toast.error("Quantidade solicitada fora do estoque!");
+            return;
+        }
+
+        setCart((prevState) => {
+            return prevState.map(item => ({
+                ...item,
+                amount: item.id === id ? amount : item.amount
+            }));
+        });
+    }, [removeFromCart]);
+
+    const addToCart = useCallback(async (id) => {
+        //Verifica se o produto já existe no carrinho
+        const productExists = cart.find((x) => x.id === id);
+
+        //consulta o stock antes de qualquer alteração
+        const { data: stock } = await api.get(`/stock/${id}`);
+
+        const stockAmount = stock.amount;
+        const currentAmount = productExists ? productExists.amount : 0;
+
+        const amount = currentAmount + 1;
+        if (amount > stockAmount) {
+            toast.error("Quantidade solicitada fora do estoque!");
+            return;
+        }
+
+        if (productExists) {
+            await updateAmount(id, amount);
+
+            toast.success("Produto adicionado ao carrinho!");
+        } else {
+            const response = await api.get(`/products/${id}`);
+
+            const data = {
+                ...response.data,
+                amount: 1, //insere quantidade 1
+                priceFormatted: formatPrice(response.data.price),
+            };
+
+            setCart(prevState => [...prevState, data]);
+
+            toast.success("Produto adicionado ao carrinho!");
+        }
+    }, [cart, updateAmount]);
+
+    return (
+        <CartContext.Provider value={{
+            cart,
+            addToCart,
+            updateAmount,
+            removeFromCart
+        }}>
+            {children}
+        </CartContext.Provider>
+    )
+} 
